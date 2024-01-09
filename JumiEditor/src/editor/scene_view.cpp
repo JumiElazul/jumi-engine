509e39d4f8700e4f3fc2fe89b0c62b6228239e16
Gregory Coomber <gr.coomber@gmail.com>
2024-01-08 17:42:48 -0800

    Working on SceneView aspect ratio

--- JumiEditor/src/editor/scene_view.cpp
+++ JumiEditor/src/editor/scene_view.cpp
@@ -38,17 +38,77 @@ namespace jumi
     {
         if (ImGui::Begin("Scene View", &_is_open, _flags))
         {
-            ImVec2 pos = ImGui::GetWindowPos();
-            int x = static_cast<int>(pos.x);
-            int y = static_cast<int>(pos.y);
-            ImVec2 size = ImGui::GetWindowSize();
-            int width = static_cast<int>(size.x);
-            int height = static_cast<int>(size.y);
+            ViewportTarget target = calculate_render_target_area();
 
             EngineCore& core = EngineCore::instance();
             Renderer& renderer = core.get_renderer();
-            renderer.set_viewport_target({ x, y, width, height });
+            renderer.set_viewport_target(target);
         }
         ImGui::End();
     }
+
+    ViewportTarget SceneView::calculate_render_target_area()
+    {
+        ImVec2 pos = ImGui::GetWindowPos();
+        int x = static_cast<int>(pos.x);
+        int y = static_cast<int>(pos.y);
+        ImVec2 size = ImGui::GetWindowSize();
+        float windowWidth = size.x;
+        float windowHeight = size.y;
+
+        float viewportWidth = 0.0f;
+        float viewportHeight = 0.0f;
+        float viewportX = 0.0f;
+        float viewportY = 0.0f;
+        float aspectRatio = 0.0f;
+
+        switch (_aspect_ratio)
+        {
+            case AspectRatio::SixteenxNine:
+            {
+                aspectRatio = 16.0f / 9.0f;
+                break;
+            }
+            case AspectRatio::SixteenxTen:
+            {
+                aspectRatio = 16.0f / 10.0f;
+                break;
+            }
+            case AspectRatio::FourxThree:
+            {
+                aspectRatio = 4.0f / 3.0f;
+                break;
+            }
+            default:
+            {
+                aspectRatio = 16.0f / 9.0f;
+                break;
+            }
+        }
+
+        if ((windowWidth / windowHeight) > aspectRatio) 
+        {
+            viewportHeight = windowHeight;
+            viewportWidth = viewportHeight * aspectRatio;
+        }
+        else 
+        {
+            viewportWidth = windowWidth;
+            viewportHeight = viewportWidth / aspectRatio;
+        }
+
+        viewportX = x + (windowWidth - viewportWidth) / 2.0f;
+        viewportY = y + (windowHeight - viewportHeight) / 2.0f;
+
+        return ViewportTarget
+        {
+            static_cast<int>(viewportX),
+            static_cast<int>(viewportY),
+            static_cast<int>(viewportWidth),
+            static_cast<int>(viewportHeight)
+        };
+    }
+
+    void SceneView::set_aspect_ratio(AspectRatio ratio) { _aspect_ratio = ratio; }
+
 }
--- JumiEditor/src/editor/scene_view.h
+++ JumiEditor/src/editor/scene_view.h
@@ -1,8 +1,16 @@
 #pragma once
 #include "editor/imgui_component.h"
+#include "EngineCore/renderer/renderer.h"
 
 namespace jumi
 {
+    enum class AspectRatio
+    {
+        SixteenxNine,
+        SixteenxTen,
+        FourxThree,
+    };
+
     class SceneView : public ImGuiComponent
     {
     public:
@@ -11,8 +19,10 @@ namespace jumi
 
         virtual void set_window_flags() override;
         virtual void render() override;
+        ViewportTarget calculate_render_target_area();
+        void set_aspect_ratio(AspectRatio ratio);
 
     private:
-
+        AspectRatio _aspect_ratio = AspectRatio::SixteenxNine;
     };
 }
